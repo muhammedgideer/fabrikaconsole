9d5336a1275a538a52a74767cf71579ae4daea89
Muhammed GİDER <74492044+muhammedgideer@users.noreply.github.com>
2020-11-17 17:39:20 +0300

    Bridge

---
+++ FaturaKopru.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    class FaturaKopru:FaturaYonetimAbstraction
+    {
+        public override void FaturaOde(Faturatip tip)
+        {
+            FaturaOdemeImplementor.Ode(tip);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FaturaYonetimAbstraction faturaYonetim =new FaturaKopru();
+            faturaYonetim.FaturaOdemeImplementor = new SuFaturasiOde();
+            faturaYonetim.FaturaOde(new Faturatip { Title = "Su Faturasi Bildirimi" });
+            faturaYonetim.FaturaOdemeImplementor = new ElektrikFaturasiOde();
+            faturaYonetim.FaturaOde(new Faturatip { Title = "Su Faturasi Bildirimi" });
+
+            Console.ReadLine();
+        }
+    }
+}
