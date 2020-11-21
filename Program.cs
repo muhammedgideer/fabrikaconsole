3c46e92614ece64b389a0fa5c06765814cea2e27
Muhammed GİDER <74492044+muhammedgideer@users.noreply.github.com>
2020-11-21 17:40:09 +0300

    Fabrika Konsol

--- Program.cs
+++ Program.cs
@@ -4,19 +4,89 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Bridge
+namespace fabrikaconsole
 {
+        public interface IArac
+        {
+            void BilgiGoster();
+        }
+
+        public class Araba : IArac
+        {
+            public void BilgiGoster()
+            {
+                Console.WriteLine("Araba Üretildi.");
+            }
+        }
+
+        public class Tir : IArac
+        {
+            public void BilgiGoster()
+            {
+                Console.WriteLine("Tır Üretildi.");
+            }
+        }
+
+        public class Motor : IArac
+        {
+            public void BilgiGoster()
+            {
+                Console.WriteLine("Motor Üretildi");
+            }
+        }
+        public enum AracTipi
+        {
+            Araba = 1,
+            Tir = 2,
+            Motor = 3
+        }
+
+        public interface IAracFabrikasi
+        {
+            IArac AracUret(AracTipi tip);
+        }
+
+        public class AracFabrikasi : IAracFabrikasi
+        {
+            public IArac AracUret(AracTipi tip)
+            {
+                IArac Arac = null;
+                switch (tip)
+                {
+                    case AracTipi.Araba:
+                        Arac = new Araba();
+                        break;
+                    case AracTipi.Tir:
+                        Arac = new Tir();
+                        break;
+                    case AracTipi.Motor:
+                        Arac = new Motor();
+                        break;
+                }
+                return Arac;
+            }
+        }
+
+
     class Program
     {
+
         static void Main(string[] args)
         {
-            FaturaYonetimAbstraction faturaYonetim =new FaturaKopru();
-            faturaYonetim.FaturaOdemeImplementor = new SuFaturasiOde();
-            faturaYonetim.FaturaOde(new Faturatip { Title = "Su Faturasi Bildirimi" });
-            faturaYonetim.FaturaOdemeImplementor = new ElektrikFaturasiOde();
-            faturaYonetim.FaturaOde(new Faturatip { Title = "Su Faturasi Bildirimi" });
 
-            Console.ReadLine();
+            var AracFabrikasi = new AracFabrikasi();
+
+            IArac aracAraba = AracFabrikasi.AracUret(AracTipi.Araba);
+            aracAraba.BilgiGoster();
+
+            IArac aracMotor = AracFabrikasi.AracUret(AracTipi.Motor);
+            aracMotor.BilgiGoster();
+            Console.Read();
+
+        }
+
+
+  
         }
     }
-}
+
